887604d5d44122bd0495471733ba67d432325d67
Adnan525 <adnaan525@gmail.com>
2023-07-15 02:19:34 +1000

    Functions added

--- Readme.md
+++ Readme.md
@@ -1,1 +1,7 @@
-"Extends App": Means the class or code between curly braces is executable. 
+"Extends App": Means the class or code between curly braces is executable.  
+Type mentioning is optional.  
+Interpolated String = s"_______________".  
+Expressions : structures that can be reduced to a value.  
+A codeblock has to return something.
+A codeblock has to return something.  
+Functions are usually recursive.
--- src/main/scala/com/scala/tutorial/Basics.scala
+++ src/main/scala/com/scala/tutorial/Basics.scala
@@ -1,6 +1,33 @@
 package com.scala.tutorial
 
 object Basics extends App{
-  val meaningOfLife : Int = 42; //constant
+  val meaningOfLife : Int = 42 //constant
+  val aBoolean : Boolean = false
+  val aString = "scala"
+  val aComposedString = "I" + " " + "love" + " " + "scala"
+  val anInterpolatedString = s"I love $aString"
+  // println(anInterpolatedString)
+  val ifExpression = if(meaningOfLife > 43) 56 else 999
 
+  //single expression
+  val chainedIfExpression = if(meaningOfLife >43) 56
+    else if(meaningOfLife < 0) -2
+    else if(meaningOfLife > 999) 78
+    else 0
+
+  //code blocks
+  val aCodeBlock = {
+    val aLocalValue: Int = 67
+    aLocalValue + 3 //return value
+  }
+
+  def myFunction(x : Int, y : String) : String = {
+    y + " " + x
+  }
+
+  def factorial(i : Int) : Int = {
+    if(i == 1) 1
+    else i * factorial(i - 1)
+  }
+  println(factorial(5))
 }
