e2a3b66654f977f23d73834b6dd8750c229622f7
Muntasir Adnan <48549305+Adnan525@users.noreply.github.com>
2023-11-15 13:47:58 +0000

    part 2 : Basics done

--- Readme.md
+++ Readme.md
@@ -1,12 +1,11 @@
-"Extends App": Means the class or code between curly braces is executable.  
-Type mentioning is optional.  
-Interpolated String = s"_______________".  
-Expressions : structures that can be reduced to a value.  
-A codeblock has to return something.
-A codeblock has to return something.  
-Functions are usually recursive.  
-In scala, don't use loops or iteration, use recursion.  
-Unit is type of side effects.  
+- "Extends App": Means the class or code between curly braces is executable, BASICALLY THE MAIN METHOD  
+- Type mentioning is optional.  
+- Interpolated String = s"_______________".  
+- Expressions : structures that can be reduced to a value.  
+- A codeblock has to return something.
+- Functions are usually recursive.  
+- In scala, don't use loops or iteration, use recursion.  
+- Unit is type of side effects.  
   
 In scala we have class, instance, inheritance, polymorphism, abstract class.  
 Class definition is also constructor.  
--- src/main/scala/com/scala/tutorial/Basics.scala
+++ src/main/scala/com/scala/tutorial/Basics.scala
@@ -1,28 +1,30 @@
 package com.scala.tutorial
 
 object Basics extends App{
-  val meaningOfLife : Int = 42 //constant
+  val meaningOfLife : Int = 42 // java : const int meaningOfLife = 42
   val aBoolean : Boolean = false
-  val aString = "scala"
+  val aString = "scala" // type mentioning is optional
   val aComposedString = "I" + " " + "love" + " " + "scala"
-  val anInterpolatedString = s"I love $aString"
+  val anInterpolatedString = s"I love $aString" // python : f"I love {aString}"
   // println(anInterpolatedString)
   val ifExpression = if(meaningOfLife > 43) 56 else 999
 
-  //single expression
+  //chaned if expression that will resut a single value or expression
   val chainedIfExpression = if(meaningOfLife >43) 56
     else if(meaningOfLife < 0) -2
     else if(meaningOfLife > 999) 78
     else 0
 
   //code blocks
-  val aCodeBlock = {
+  // the Int is optional but it shows the variable will hold a single value only, not the whole funnction
+  val aCodeBlock : Int= {
     val aLocalValue: Int = 67
     aLocalValue + 3 //return value
-  }
+  } 
+
 
   def myFunction(x : Int, y : String) : String = {
-    y + " " + x
+     y + " " + x
   }
 
   def factorial(i : Int) : Int = {
@@ -31,10 +33,11 @@ object Basics extends App{
   }
   println(factorial(5))
 
-  //unit return type
-  //unit == void
-  //type of side effects
+  // unit return type
+  // unit == void
+  // side effects
   def unitReturningFunction() : Unit = {
     println("side effect return type")
   }
+  unitReturningFunction()
 }
